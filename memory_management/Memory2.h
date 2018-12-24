339d9798dc6c296112d498c13e0ab8cf6364276d
Zha2ha <2715202327@qq.com>
2018-12-24 21:37:02 +0800

    页面结构体

--- memory_management/Memory2.h
+++ memory_management/Memory2.h
@@ -4,9 +4,9 @@ struct Frame
 {
 	bool Isempty;//是否被占用标志
 	int Time;//占用时长
-	int start;
-	int end;
-	string Pname;
+	int start;//起始地址
+	int end;//结束地址
+	string Pname;//进程名
 };
 void init_frame(Frame F[]);
 void AddProcess(Frame F[], int &count);
